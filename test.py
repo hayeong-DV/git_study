98dab9a15ed23b2fb41ba291201c71cfabdb9d50
hayeong-DV <jh0worl904@gmail.com>
2022-05-17 09:24:27 +0900

    충돌을 내보자-mainwq

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-q1qq2#feature
-print("feature브랜치에서 만든파일")
-print("기능 하나 추가했다고 가정")
-
-#develop
-print("기능개발 완료")
-
-
-#fetch테스트 -3
-print("왜 안되냐")
-#test-3
-
-#git log 테스트 -main에서 작성2
-#test 브랜치에서 수정1
+#충돌을 내보자-main
